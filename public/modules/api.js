a7fc3fc916db4058d1b5c01e3455ba48687089e3
moguchev <abracadabra.14@mail.ru>
2019-09-29 21:18:26 +0200

    api modified

--- public/modules/api.js
+++ public/modules/api.js
@@ -46,6 +46,29 @@ export default class Api {
     }
 
     /**
+     * Регистрирует соискателя
+     * @param ...
+     * @returns {Promise<Response>}
+     */
+    static signUpEmployer ({ CompanyName, Site, FirstName, SecondName, Email,  phoneNumber, ExtraNumber,  Password, City, EmplNum } = {}) {
+        return Net.doPost({
+            url: '/employer',
+            body: {
+                "company_name": CompanyName,
+                "site": Site,
+                "first_name": FirstName,
+                "second_name": SecondName,
+                "email": Email,
+                "phone_number": phoneNumber,
+                "extra_number": ExtraNumber,
+                "password": Password,
+                "city": City,
+                "empl_num": EmplNum,
+            }
+        });
+    }
+
+    /**
      * Запрос данных для профиля соискателя
      * @param email
      * @param firstName
@@ -71,6 +94,98 @@ export default class Api {
         });
     }
 
+     /**
+     * Создание резюме
+     * @param ResumeData
+     * @returns {Promise<Response>}
+     */
+    // Надо сделать модель Resume(пока что просто поля через запятую)
+    static createResume ({ FirstName, SecondName, City, PhoneNumber, BirthDate,  Sex, Citizenship, Experience, Profession, Position, Wage, Education, About} = {}) {
+        return Net.doPost({
+            url: '/resume',
+            body : {
+                "first_name": FirstName,
+                "second_name": SecondName,
+                "city": City,
+                "phone_number": PhoneNumber,
+                "birth_date": BirthDate,
+                "sex": Sex,
+                "citizenship":  Citizenship,
+                "experience": Experience,
+                "profession": Profession,
+                "position": Position,
+                "wage":   Wage,
+                "education":  Education,
+                "about":  About,
+            }         
+        });
+    }
+
+    /**
+     * Создание вакансии
+     * @param VacancyData
+     * @returns {Promise<Response>}
+     */
+    // Надо сделать модель Resume(пока что просто поля через запятую)
+    static createResume ({ CompanyName, Experience, Profession, Position, Tasks, Requirements, Wage, Conditions, About } = {}) {
+        return Net.doPost({
+            url: '/vacancy',
+            body : {
+                "company_name": CompanyName,
+                "experience": Experience,
+                "profession": Profession,
+                "position":  Position,
+                "task": Tasks,
+                "requirements": Requirements,
+                "wage": Wage,
+                "conditions": Conditions,
+                "about": About,
+            }         
+        });
+    }
+
+    /**
+     * Запрос списка резюме
+     * @param ...
+     * @returns {Promise<Response>}
+     */
+    static getResumes() {
+        return Net.doGet({
+            url: '/resumes',
+        });
+    }
+
+    /**
+     * Запрос списка вакансий
+     * @param ...
+     * @returns {Promise<Response>}
+     */
+    static getVacancies() {
+        return Net.doGet({
+            url: '/vacancies',
+        });
+    }
+
+    /**
+     * Запрос данных резюме
+     * @param Id
+     * @returns {Promise<Response>}
+     */
+    static getResumeById( id = {}) {
+        return Net.doGet({
+            url: '/resume' + '/' + id
+        });
+    }
+
+    /**
+     * Запрос данных вакансии
+     * @param Id
+     * @returns {Promise<Response>}
+     */
+    static getVacancyById( id = {}) {
+        return Net.doGet({
+            url: '/vacancy' + '/' + id
+        });
+    }
+
 }
-
-
