e64048713a6b29d00b2bac4e080d4d4e714258fe
Vladimir <vovasukiasyan@gmail.com>
2019-09-30 00:14:20 +0300

    Add SignupMenu

---
+++ public/components/SignupMenu/SignupMenu.js
@@ -0,0 +1,12 @@
+import template from './signupmenu.pug'
+
+export class SignupMenuComponent{
+    constructor (parent = document.body, data = {}) {
+        this._parent = parent;
+        this._data = data;
+    }
+
+    render () {
+        this._parent.innerHTML = template(this._data);
+    }
+}
