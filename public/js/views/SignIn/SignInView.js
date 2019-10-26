7791091286a15bcf616c213f4977f5ab745eeb18
moguchev <abracadabra.14@mail.ru>
2019-10-26 15:11:15 +0200

    bem architecture of css

--- public/js/views/SignIn/SignInView.js
+++ public/js/views/SignIn/SignInView.js
@@ -3,45 +3,47 @@ import { View } from '../../modules/view';
 import Validation from '../../modules/validation';
 
 export class SignInView extends View {
-
-  constructor (root, eventBus) {
+  constructor(root, eventBus) {
     super(root, template, eventBus);
     this._eventBus.subscribeToEvent('signInFailed', this._onSubmitFailed.bind(this));
   }
 
-  render (data = {}) {
+  render(data = {}) {
     super.render(data);
 
-    this._loginForm = this._root.querySelector('.login__form');
+    this._loginForm = this._root.querySelector('.login__form-js');
     this._loginForm.addEventListener('submit', this._onSubmit.bind(this), false);
 
     this.setValidationListeners();
   }
 
-  setValidationListeners () {
+  setValidationListeners() {
     const email = this._loginForm.elements['email'];
 
-    email.addEventListener('input', function (event) {
-      const notValid = Validation.validateEmail(event.target.value, true);
-      const error = event.target.nextElementSibling;
-      if (Validation.isEmptyField(event.target.value) || !notValid) {
-        event.target.className = 'input';
-        error.innerHTML = '';
-        error.className = 'error';
-      } else {
-        event.target.className = 'input invalid';
-        error.innerHTML = 'Некорректный email';
-      }
-    }, false);
+    email.addEventListener(
+      'input',
+      function(event) {
+        const notValid = Validation.validateEmail(event.target.value, true);
+        const error = event.target.nextElementSibling;
+        if (Validation.isEmptyField(event.target.value) || !notValid) {
+          error.innerHTML = '';
+          error.classList.remove('error_active');
+        } else {
+          error.innerHTML = 'Некорректный email';
+          error.classList.add('error_active');
+        }
+      },
+      false
+    );
   }
 
-  _onSubmitFailed (data) {
-    const error = this._root.querySelector('.error-block');
-    error.classList.add('error-block_login');
-    error.innerHTML = `<p>${data.error}</p>`;
+  _onSubmitFailed(data) {
+    const error = this._root.querySelector('.light-page__error-js');
+    error.classList.add('light-page__error_active');
+    error.innerHTML = `<p>${data.error}<p>`;
   }
 
-  _onSubmit (ev) {
+  _onSubmit(ev) {
     ev.preventDefault();
     let wasfail = false;
 
@@ -53,14 +55,12 @@ export class SignInView extends View {
       if (Validation.isEmptyField(input.value)) {
         const error = input.nextElementSibling;
         error.innerHTML = 'Обязательное поле';
-        error.className = 'error active';
-        input.className = 'input invalid';
+        error.classList.add('error_active');
         wasfail = true;
       } else {
         const error = input.nextElementSibling;
         error.innerHTML = '';
-        error.className = 'error';
-        input.className = 'input';
+        error.classList.remove('error_active');
       }
     });
 
@@ -69,7 +69,7 @@ export class SignInView extends View {
     } else {
       const user = {
         email: email.value,
-        password: password.value
+        password: password.value,
       };
       this._eventBus.triggerEvent('signIn', user);
     }
