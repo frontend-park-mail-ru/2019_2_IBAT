0cedb5e0c56ae90d649f52a2b58d312fb35ca4e2
moguchev <abracadabra.14@mail.ru>
2019-12-24 14:53:49 +0200

    просмотр откликнувшихся на вакансию

--- public/js/views/VacancyPage/VacancyPageView.js
+++ public/js/views/VacancyPage/VacancyPageView.js
@@ -2,13 +2,13 @@ import template from './vacancyPage.pug';
 import { View } from '../../modules/view';
 import { ACTIONS, AUTH, VACANCY } from '../../modules/events';
 import { Api } from '../../modules/api';
+import { ShortResumeComponent } from '../../../components/ShortResume/ShortResume';
 
 export class VacancyPageView extends View {
 
   constructor (root, globalEventBus) {
     super(root, template, globalEventBus);
 
-    this._globalEventBus.subscribeToEvent(AUTH.checkAuthResponse, this._onAuthResponse.bind(this));
     this._globalEventBus.subscribeToEvent(VACANCY.getVacancySuccess, this._onGetVacancySuccess.bind(this));
     this._globalEventBus.subscribeToEvent(VACANCY.getVacancyFailed, this._onGetVacancyFailed.bind(this));
   }
@@ -16,19 +16,6 @@ export class VacancyPageView extends View {
   render (data = {}) {
     this.merge(data);
     this.isViewClosed = false;
-
-    this._globalEventBus.triggerEvent(AUTH.checkAuth);
-  }
-
-  /**
-   * Получает данные авторизации(роль юзера) и запрашивает вакансию
-   * @param data
-   * @private
-   */
-  _onAuthResponse (data) {
-    if (this.isViewClosed) {
-      return;
-    }
 
     this._globalEventBus.triggerEvent(VACANCY.getVacancy, this.data.id);
   }
@@ -47,7 +34,67 @@ export class VacancyPageView extends View {
     this.data['wage_from'] = this.data['wage_from'].split('.')[0];
     this.data['wage_to'] = this.data['wage_to'].split('.')[0];
     this.data['seeker'] = localStorage.getItem('role').match(/seeker/);
-    super.render(this.data);
+
+    Api.getProfile()
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error(res.text);
+        }
+      })
+      .then(profileData => {
+        let my_id = profileData.profile.id;
+        this.data = { ...this.data, my_id };
+        super.render(this.data);
+        return document.querySelector('.company__vacancies-list');
+      })
+      .then(list => {
+        if (list) {
+          this._list = list;
+          console.log(this.data.id);
+          return Api.getResponds(undefined, this.data.id);
+        }
+      })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error(res.text);
+        }
+      })
+      .then(responds => {
+        console.log(responds);
+        let query = '?';
+        responds.forEach((r, i) => {
+          query += `id${i}=${r.resume_id}&`;
+        });
+        if (query != '?') {
+          console.log(query);
+          return Api.getResumesByIds(query);
+        }
+      })
+      .then(res => {
+        if (res) {
+          if (res.ok) {
+            return res.json();
+          }
+        }
+      })
+      .then((resumes = []) => {
+        console.log(resumes);
+        if (resumes.length) {
+          resumes.forEach(resume => {
+            new ShortResumeComponent({resume}).appendTo(this._list);
+          })
+        } else {
+          this._list.innerHTML = '<p>На эту вакансию ещё никто не откликнулся.'
+        }
+      })
+      .catch(error => {
+        console.error(error);
+        super.render(this.data);
+      });
   }
 
   onRender () {
