11d13823af02d1757239e4da41d45a429286b717
vladimirsukiasyan <vova_1999-99@mail.ru>
2020-02-20 00:55:35 +0300

    Some changes

--- public/js/modules/net.js
+++ public/js/modules/net.js
@@ -1,10 +1,10 @@
-export const host = 'tko.vladimir.fvds.ru';
+export const host = '84.201.173.146';
 
-export const serverURL = `https://${host}/api`;
-export const serverNotificationURL = `wss://${host}/api/notifications`;
-export const serverImgURL = `https://${host}/img`;
-export const serverSupportChatURL = `wss://${host}:443/chat`;
-export const serverChatURL = `wss://${host}:443/api/chat/ws`;
+export const serverURL = `http://${host}/api`;
+export const serverNotificationURL = `ws://${host}/api/notifications`;
+export const serverImgURL = `http://${host}/img`;
+export const serverSupportChatURL = `ws://${host}:443/chat`;
+export const serverChatURL = `ws://${host}:443/api/chat/ws`;
 
 export default class Net {
   /**
