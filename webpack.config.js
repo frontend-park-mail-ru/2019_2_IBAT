eb4e829ff7d109e49ff0e60b8fd0d1ef996780bb
lamoguch <lamoguch@mts.ru>
2020-12-01 19:26:50 +0300

    .

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       inject: true,
-      title: 'IBAT'
+      title: 'MyJobs'
     }),
     new MiniCssExtractPlugin({}),
     new ServiceWorkerWebpackPlugin({
